3d41e089d2c2b8dfe6176fc1f144e8d44f3ba5df
Vladyslav <vlad30vv@gmail.com>
2020-05-17 19:41:24 +0300

    stopSubmit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import { required } from "../../utils/validators/validators";
 import { connect } from "react-redux";
 import { login } from "../../redux/auth-reducer";
 import { Redirect } from "react-router-dom";
+import style from "./../common/Preloader/FormControls/FormControl.module.css"
 
 const LoginForm = (props) => {
   return (
@@ -30,6 +31,11 @@ const LoginForm = (props) => {
         <Field component={Input} name={"rememberMe"} type={"checkbox"} />
         remember me
       </div>
+      {
+      props.error && <div className={style.formSummatyError}>
+{props.error}
+      </div>
+      }
       <div>
         <button>Login</button>
       </div>
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,4 +1,7 @@
-import {authAPI} from "./../api/api"
+import {authAPI} from "./../api/api";
+import {stopSubmit} from "redux-form";
+
+
 const SET_USERS_DATA = "SET_USERS_DATA";
 
 
@@ -37,10 +40,14 @@ export const getAuthUserData = () => (dispatch) => {
 }
 
 export const login = (email, password, rememberMe) => (dispatch) => {
+   
   authAPI.login(email, password, rememberMe) 
   .then((response) => {
    if (response.data.resultCode === 0) {
      dispatch(getAuthUserData())
+    } else {
+     let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error";
+      dispatch(stopSubmit("login", {_error: message}));
    }
   });
 }
